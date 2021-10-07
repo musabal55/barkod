be4468a39b752adc447ade025a234d9c7f061250
musabal <musahoney@gmail.com>
2021-10-08 00:19:55 +0300

    first commit

---
+++ lib/barcode.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+  Future<String> scanQR() async {
+    String barcodeScanRes;
+
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      print(barcodeScanRes);
+      return barcodeScanRes;
+  }
+
+
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import './barcode.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Barcode(),
+  ));
+}
+
+
+class Barcode extends StatefulWidget {
+  @override
+  _BarcodeState createState() => _BarcodeState();
+}
+
+class _BarcodeState extends State<Barcode> {
+  String? qrKod;
+
+  Future<void> fetchQR()async {
+    var kod = await scanQR();
+    setState(() {
+      qrKod = kod;
+    });
+  }
+
+  @override
+
+  void initState(){
+    fetchQR();
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return
+      MaterialApp(
+        home :Scaffold(
+        appBar: AppBar(
+          title: Text("BARKOD UYGULAMASI"),
+          centerTitle: true,
+        ),
+        body: Container(
+          margin: EdgeInsets.all(90),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              FlatButton(           // barkod okuma kodları buraya
+                onPressed:(){
+                  scanQR();
+
+                },
+                child: Text("Barkod okut"),
+                color: Colors.grey[400],
+              ),
+              //scanQR() != "" ? print("dolu") : print("boş");
+              Text(qrKod==null?"bekle":qrKod.toString()),            // barkoddan dönen değer buraya
+
+            ],
+          ),
+        ),
+    ),
+      );
+  }
+}
+
+// https://zahidtekbas.com.tr/2021/01/03/flutter-future-nedir-nasil-kullanilir/
